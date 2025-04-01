c35336b073e4ebc26ee9ec6f2f43bc7367b99346
Shivanandballi09 <shivanandballi264@gmail.com>
2025-04-01 13:26:07 +0530

    Create devopstest.c

---
+++ devopstest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input from the user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Performing addition
+    sum = num1 + num2;
+
+    // Displaying the result
+    printf("The sum is: %d\n", sum);
+
+    return 0;
+}
